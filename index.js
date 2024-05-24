508d719fa1e1847b079a579f5855fb2477b95c71
Usuario GitLab <aldoulaguent@gmail.com>
2024-05-23 21:14:39 -0500

    feat: Practica 07

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+const server = express();
+
+const port = 8080;
+
+server.use(express.json);
+
+const koders = [];
+
+//Registrar
+const registrar = (req, res) => {
+    const newKoder = req.body.koder
+    if (!newKoder) {
+        res.status(400)
+        res.json({
+            message: 'koder is required'
+        })
+        return;
+    }
+    alls.push(newKoder)
+    res.json({
+        message: 'koder is added',
+        koders: koders
+    })
+}
+
+//Listar 
+const listar = (req, res) => {
+    res.json({
+        message: 'KodersAll',
+        koders: koders
+    })
+}
+
+//Eliminar por nombre
+const eliminarPorNombre = (req, res) => {
+    const idElminimar = req.params.id;
+    const idNumero = parseInt(idElminimar);
+
+    if (!Number(idNumero)) {
+        res.status(400)
+        res.json({
+            message: 'invalid'
+        })
+        return;
+    }
+
+    if (idNumero < 0 || idNumero >= koders.length) {
+        res.status(400)
+        res.json({
+            message: 'invalid'
+        })
+        return;
+    }
+
+    koders.splice(idNumero, 1)
+    res.json({
+        message: 'Eliminado/s'
+    })
+}
+
+server.get('/koders', registrar)
+server.get('/koders', listar)
+server.get('/koders/:id', eliminarPorNombre)
+
+
+server.listen(port, () => {
+    console.log("Server runing")
+})
